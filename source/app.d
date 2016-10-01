47c5dbd8ee9cfafd69a88d9df0cac233ae83bcc5
Mathias Baumann <marenz@supradigital.org>
2016-10-01 13:36:36 +0200

    Simple AND/OR network

---
+++ source/app.d
@@ -0,0 +1,104 @@
+import std.stdio;
+import mir.ndslice;
+import mir.glas.common;
+
+GlasContext glas;
+
+
+void dot ( Matrix2d mat, Vector vec, ref Vector result )
+{
+    import mir.glas.l2;
+
+    gemv!(double, double, double)(&glas, 1.0L, mat, vec, 0.0, result);
+}
+
+
+auto sigmoid ( T ) ( T x )
+{
+    import std.math : exp;
+
+    return 1.0 / (1.0 + exp(-x));
+}
+
+
+auto sigmoid_derived ( T ) ( T x )
+{
+    return x * (1.0 - x);
+}
+
+// Create a two dimensional array filled with random values
+auto randomSlice ( Lengths... ) ( double min, double max, Lengths lengths )
+{
+    import std.random;
+
+    auto matrix = slice!double(lengths);
+
+    matrix.ndEach!((ref a) => a = uniform(min, max));
+
+    return matrix;
+}
+
+alias Matrix2d = Slice!(2, double*);
+alias Vector = Slice!(1, double*);
+
+
+
+void main()
+{
+    auto x = slice!double([4, 3]);
+
+    x[0][] = [0.0, 0.0 ,1.0];
+    x[1][] = [0.0, 1.0 ,1.0];
+    x[2][] = [1.0, 0.0 ,1.0];
+    x[3][] = [1.0, 1.0 ,1.0];
+
+    import mir.ndslice.iteration;
+
+    auto y = slice!double([4, 1]);
+    y.transposed[] = [0.0, 0.0, 0.0, 1.0];
+
+    auto syn0 = randomSlice(-1.0, 1.0, 3);
+
+	writefln("my x: %s\nmy y: %s\nsyn0: %s", x, y, syn0);
+
+    auto l1_error = slice!double(y.shape);
+    auto l1_delta = slice!double(l1_error.shape);
+
+    auto dot_result = slice!double(syn0.shape);
+
+    Vector l1 = slice!double([4]);
+    foreach (iter; 0..1000)
+    {
+        auto l0 = x;
+
+        l0.dot(syn0, l1);
+
+        //writefln("%s dot %s = %s", l0, syn0, l1);
+
+        l1.ndEach!((ref a)=>a=sigmoid(a));
+
+        l1_error[] = y;
+        l1_error.transposed[] -= l1;
+
+        //writefln("SYN0 : %s", syn0);
+       // writefln("ERROR FOUND: %s", l1_error);
+
+        l1_delta[] = l1_error;
+
+        auto zip = assumeSameStructure!("l1_delta",
+                "l1")(l1_delta.transposed[][0],
+                    l1);
+
+        zip.ndEach!((z) { //writefln("l1_delta %s, l1: %s", z.l1_delta, z.l1);
+                z.l1_delta = z.l1_delta * sigmoid_derived(z.l1); });
+
+        writefln("l1 delta: %s", l1_delta);
+
+        l0.transposed.dot(l1_delta.transposed[][0], dot_result);
+
+        syn0[] += dot_result;
+
+        writefln("RESULT: %s", l1);
+    }
+
+}
